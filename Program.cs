bbf343e875c44c53a649109f465aa646221c3bfa
bkid00 <103415536+bkid00@users.noreply.github.com>
2022-10-09 23:43:31 +0530

    Added a c# program

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            //Console.WriteLine("Hello World");
+
+            //---Drawing a Shape---------
+            //Console.WriteLine("    /|");
+            //Console.WriteLine("   / |");
+            //Console.WriteLine("  /  |");
+            //Console.WriteLine(" /___|");
+
+            //-------Variables------------
+            //string characterName = "John";
+            //int characterAge;
+            //characterAge = 35;
+            //Console.WriteLine("There once was a man named " + characterName);
+            //Console.WriteLine("He was " + characterAge +" years old");
+            //characterName = "Mike";
+            //Console.WriteLine("He really liked the name " + characterName);
+            //Console.WriteLine("But didn't like being " + characterAge);
+
+
+            //Data Types
+            String phrase = "Bhagya Kithmini";
+            Char grade = 'C';
+            int age = 30;
+            double gpa = 3.3;
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
